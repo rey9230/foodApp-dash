d52564183bde8a1c3730aa099f4aa12c3d6356d6
Reniel Gonzalez <iamrenielgonzalez@gmail.com>
2024-01-10 04:58:01 -0500

    setting home page

--- src/router.tsx
+++ src/router.tsx
@@ -83,7 +83,7 @@ const routes: RouteObject[] = [
     children: [
       {
         path: '/',
-        element: <Overview />
+        element: <Navigate to="dashboards/crypto" replace/> //element: <Overview />
       },
       {
         path: 'overview',
